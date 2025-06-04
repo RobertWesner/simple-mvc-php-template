781bbca7fd9f367720c848baa4202881dd220870
RobertWesner <robert@wesner.io>
2025-06-04 18:34:08 +0200

    Update to MVC v0.10.0

--- route.php
+++ route.php
@@ -1,24 +1,15 @@
 <?php
 
 /**
- * @version v0.9.0
+ * @version v0.10.0
  */
 
 declare(strict_types=1);
 
-use RobertWesner\SimpleMvcPhp\ErrorRenderer;
-use RobertWesner\SimpleMvcPhp\Routing\RouterFactory;
+use RobertWesner\SimpleMvcPhp\MVC;
 
 require __DIR__ . '/vendor/autoload.php';
 
 const __BASE_DIR__ = __DIR__;
 
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-try {
-    echo RouterFactory::createRouter()->route($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
-} catch (Throwable $exception) {
-    echo ErrorRenderer::render($exception);
-}
+MVC::route();
